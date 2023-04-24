424e54c05949e3e857047ab2bf0f2fafc5a13d68
YuvrajKatkar <yuvrajkatkar.2000@gmail.com>
2023-04-24 14:39:54 +0530

    Added the methods to reserve a table

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 class Main {
     public static void main(String[] args) {
         HotelManager hm = new HotelManager();
+        Hotel h = new Hotel();
+        h.tables[0]=new Table(false);
+        h.tables[1]=new Table(false);
+        h.tables[2]=new Table(false);
+        h.tables[3]=new Table(false);
+        h.tables[4]=new Table(false);
+
         while(true) {
-            System.out.println("1. Select Hotel 2. OrderFood 3. Bill 4. Payment 5. exit");
+            System.out.println("1. Select Hotel 2. OrderFood 3. Bill 4. Payment \n5. Customer Entered in hotel 6. Customer left table 7.exit");
             switch (hm.sc.nextInt()){
                 case 1: {
                     hm.chooseHotel();
@@ -15,7 +22,9 @@ class Main {
                 }break;
                 case 4: hm.payment();
                     break;
-                case 5:
+                case 5: h.CustomerEntered();break;
+                case 6: h.RemoveCustomerFromTable();break;
+                case 7:
                     System.out.println("Application is closed");
                     return;
                     //Instead of closing the entire application we choose to return the flow to jvm(caller)
